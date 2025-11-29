bc59e7262bc3b564f48f95c3295092e56b53b3ed
maham mansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-11-29 22:39:13 +0500

    waving and killing enemiesssss

--- src/wave.py
+++ src/wave.py
@@ -18,3 +18,38 @@ class Wave:
 
     def update(self, delta):
         self.enemies.update(delta)
+
+        self.spawn_time -= delta
+        if self.spawn_time > 0:
+            return
+
+        if self.started and self.spawn_count_small > 0:
+            self.spawn("enemy_small")
+            self.spawn_count_small -= 1
+        
+        if self.started and self.spawn_count_medium > 0 and self.spawn_count_small <= self.spawn_count_medium:
+            self.spawn("enemy_medium")
+            self.spawn_count_medium -= 1
+        
+        if self.started and self.spawn_count_large > 0 and self.spawn_count_medium <= self.spawn_count_large:
+            self.spawn("enemy_large")
+            self.spawn_count_large -= 1
+            
+        if not self.started:
+            self.started = True
+
+        self.spawn_time = self.spawn_gap
+
+    def spawn(self, enemy_type):
+        """
+        Spawns enemyyyy
+        """
+        enemy = Enemy(self.game, enemy_type, 0, 0)
+        self.enemies.add(enemy)
+
+    def enemy_killed(self):
+        """
+        whenever enemy killed.
+        """
+        if len(self.enemies) == 0 and self.spawn_count_small <= 0 and self.spawn_count_medium <= 0:
+            self.done = True
