e77449d010341a3fc4039f4e2e228d21ac92b921
maham mansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-11-30 12:58:57 +0500

    main workkkks

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from core.game import Game
+from core.window import Window
+
+#init pygame
+pygame.init()
+
+#create window
+window = Window(1280, 768)
+window.set_title("Bow Busters")
+window.set_background(148, 168, 176)
+
+#create game instance
+game = Game(window)
+game.run()
+
+#quit pygame
+pygame.quit()
