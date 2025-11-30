5397ea88a68dbf0840151ff477fd9ae82f103da1
sumayya-emaan <semaan.bscs24seecs@seecs.edu.pk>
2025-11-30 18:45:23 +0500

    menu.py basic structure

---
+++ src/menu.py
@@ -0,0 +1,21 @@
+from core.prefab import Prefab
+from core.leaderboard import Leaderboard
+from pygame.sprite import OrderedUpdates
+import pygame
+import math
+
+
+class Menu(Prefab):
+    """
+    Controls the menu system.
+    """
+
+    
+
+   
+class MenuLabel(Prefab):
+    """
+    A label displayed by the menu system. Contains a background.
+    """
+
+    
