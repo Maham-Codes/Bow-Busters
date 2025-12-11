1fe15e5e380bb88a8a1daa2c8e07be5b34d88e92
Arham-Fatima0 <arhamfatima2525@gmail.com>
2025-12-12 00:03:53 +0500

    yes yes it is done FINALLY

--- src/pathfinding.py
+++ src/pathfinding.py
@@ -27,8 +27,7 @@ class Pathfinding:
         """
         for i in range(count):
             self.pool.append(Path(self, self.find_start()))
-            
-            
+
     def find_start(self):
         """
         Finds a start point for a full length path.
@@ -48,7 +47,6 @@ class Pathfinding:
         # No start found, supply a default.
         return (x, random.randint(0, cells - 1) * self.collision.tile_size)
 
-    
     def get_point_usage(self, point):
         """
         Returns the number of existing paths that use the given point.
@@ -87,7 +85,7 @@ class Pathfinding:
                 return path
 
         return self.get_partial_path(self.find_start())[0]
-    
+
     def repair(self, point):
         """
         Called when a point has been blocked by a turret.
@@ -134,7 +132,8 @@ class Pathfinding:
                 return False
 
         return True
- 
+
+
 class Path:
     """
     A single path across the level.
@@ -219,88 +218,98 @@ class Path:
 
                 if not exists:
                     self.open_set.add(neighbour)
-                    
-    class Path:
-    """
-    A single path across the level.
-    Calculated using the A* pathfinding algorithm across multiple frames.
-    Can be repaired if one of its points becomes blocked.
-    """
 
-    def __init__(self, pathfinding, start):
-        """ 
-        Constructor. 
+    def get_lowest_score(self, open_set, scores):
+        """
+        Finds the point with the lowest score.
         ...
         """
-        self.start = start
-        self.pathfinding = pathfinding
-        self.collision = self.pathfinding.collision
-        self.res = self.collision.tile_size
-        self.points = None
-        self.start_search()
+        lowest_score = 999999999
+        lowest_point = (0, 0)
 
-    def next(self, current):
+        for p in open_set:
+            score = scores[p]
+
+            if lowest_score > score:
+                lowest_score = score
+                lowest_point = p
+
+        return lowest_point, lowest_score
+
+    def get_neighbours(self, position):
         """
-        Attempts to gets the next point in the path.
+        Finds a list of neighbouring tiles for the given position.
         ...
         """
-        if current not in self.points:
-            return False
+        if position[0] >= self.pathfinding.game.window.resolution[0]:
+            return [(position[0] - self.res, position[1])]
 
-        index = self.points.index(current)
-        length = len(self.points)
+        x_diff = range(position[0] - self.res, position[0] + self.res + 1, self.res)
+        y_diff = range(position[1] - self.res, position[1] + self.res + 1, self.res)
 
-        if index + 1 == length:
-            return False
-
-        return self.points[index + 1]
-
-    def start_search(self):
+        return [(x, y) for x in x_diff for y in y_diff if (x, y) != position and (x == position[0] or y == position[1] or self.can_use_diagonal(position, (x, y))) and not self.collision.point_blocked(x, y)]
+        
+    def can_use_diagonal(self, a, b):
         """
-        (Re)starts the pathfinding search.
+        Returns true if the diagonal between a and b is clear.
+        ...
         """
-        self.done = False
-        self.closed_set = set()
-        self.open_set = {self.start}
-        self.scores = {self.start: 0}
-        self.came_from = { }
+        return not self.collision.point_blocked(b[0], a[1]) and not self.collision.point_blocked(a[0], b[1])
 
-    def search(self):
+    def get_cost(self, a, b):
         """
-        Starts or continues an A* search for an apropriate path.
+        Calculates the cost of moving between the given positions.
+        ...
         """
-        iterations = 25
-        while len(self.open_set) > 0 and iterations > 0:
-            iterations -= 1
+        base = 3 if a[0] == b[0] or a[1] == b[1] else 4
+        crowding = self.pathfinding.get_point_usage(b)
 
-            # Find the next node to evaluate.
-            current, current_score = self.get_lowest_score(self.open_set, self.scores)
+        return base + crowding
 
-            # Check if it is a destination
-            if current[0] < 0:
-                self.points = self.trace_path(current, self.came_from)
-                self.done = True
+    def trace_path(self, current, came_from):
+        """
+        Traces a finished path from finish to start.
+        ...
+        """
+        path = [ current ]
+        while current in came_from:
+            current = came_from[current]
+            path.insert(0, current)
+
+        return path
+
+    def repair(self, point):
+        """
+        Attempts to repair a path after a point is blocked.
+        ...
+        """
+        index = self.points.index(point)
+
+        if index != 0 and index < len(self.points) - 1:
+            previous = self.points[index - 1]
+            next = self.points[index + 1]
+
+            previous_neighbours = self.get_neighbours(previous)
+            next_neighbours = self.get_neighbours(next)
+
+            # If next and previous are adjacent, just remove the point.
+            if next in previous_neighbours:
+                self.points.remove(point)
                 return
 
-            # Remove from the open set.
-            self.open_set.remove(current)
+            # If not, check for a common neighbour.
+            for neighbour in previous_neighbours:
+                if neighbour in next_neighbours:
+                    self.points[index] = neighbour
+                    return
 
-            # Add to the closed set.
-            self.closed_set.add(current)
+            # If not, check neighbours of neighbours.
+            for neighbour in previous_neighbours:
+                for neighbour_neighbour in self.get_neighbours(neighbour):
+                    if neighbour_neighbour in next_neighbours:
+                        self.points[index] = neighbour
+                        self.points.insert(index + 1, neighbour_neighbour)
+                        return
 
-            # Consider each neighbour.
-            for neighbour in self.get_neighbours(current):
-
-                # Skip if already in the closed set
-                if neighbour in self.closed_set:
-                    continue
-
-                score = current_score + self.get_cost(current, neighbour)
-                exists = (neighbour in self.open_set)
-
-                if not exists or self.scores[neighbour] > score:
-                    self.scores[neighbour] = score
-                    self.came_from[neighbour] = current
-
-                if not exists:
-                    self.open_set.add(neighbour)
+        # No solution, remake path.
+        self.start_search()
